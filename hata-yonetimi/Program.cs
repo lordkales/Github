19007e276f4435230020f4d7ac14e78d1ee72e08
Burak <akifboylu_93@hotmail.com>
2022-12-29 14:50:41 +0300

    hata yönetimi try catch finally

---
+++ hata-yonetimi/Program.cs
@@ -0,0 +1,38 @@
+﻿try{
+Console.WriteLine("Sayı Girin: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sayı Budur: " + sayi);
+}
+catch(Exception ex)
+{
+Console.WriteLine(ex.Message.ToString());
+}
+finally
+{
+    Console.WriteLine("Tamamlandı");
+}
+
+//Null Değer Hatasını İnceleyelim;
+
+Console.WriteLine("Null Değer Hatasını İnceleyelim;");
+
+try
+{
+    //int a = int.Parse("ali");
+    int a = int.Parse("-20000000000");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("Boş Değer Girdiniz: "+ ex);
+}
+catch(FormatException ex)
+{
+    Console.WriteLine("Format Hatası");
+}
+catch(OverflowException ex)
+{
+    Console.WriteLine("Küçük yada büyük değer girdiniz.");
+}
+finally{
+    Console.WriteLine("işlem tamamlandı");
+}
