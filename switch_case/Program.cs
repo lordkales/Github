4c7a0a9ae0f455b4aaa753c36ea3c94451a90457
lordkales <akifboylu_93@hotmail.com>
2022-12-30 23:34:01 +0300

    switch case dersi

---
+++ switch_case/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int ay = DateTime.Now.Month;
+        
+        switch (ay)
+        {
+            case 1:
+                Console.WriteLine("Ocak Ayındasınız");
+                break;
+            case 12:
+            Console.WriteLine("Aralık Ayındasınız");
+            break;
+
+        }
+    }
+}
