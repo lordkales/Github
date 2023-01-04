5421a6eaf59024d3c21f3c6671662c6b1a799e68
lordkales <akifboylu_93@hotmail.com>
2023-01-04 06:49:04 +0300

    while foreach dersi tamamlandı

---
+++ while_foreach/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+     //1'den başlayarak console'dan girilen sayıya kadar ortalama hesaplayıp yazdır
+
+     //Console.WriteLine("Sayı Giriniz: ");
+     //int sayi = int.Parse(Console.ReadLine());
+     //int toplam=0;
+     //int sayac=1;
+     //while (sayac<=sayi)
+     //{
+     //    toplam+=sayac;
+     //    sayac++;
+     //}
+     //Console.WriteLine(toplam/sayi);
+
+     ////a'dan z'ye kadar tüm harfleri konsola yazdır
+
+     //char harf = 'a';
+
+     //while (harf <= 'z')
+    {//
+     //Console.Write(harf);
+     //harf++;
+    }
+
+    //FOREACH
+        Console.WriteLine("*** FOREACH ***");
+        string[] arabamarkalari={"BMW", "FORD", "VOLVO", "MERCEDES", "TOYOTA"};
+        foreach (var markalar in arabamarkalari)
+        {
+            Console.WriteLine(markalar);
+        }
+    }
+}
