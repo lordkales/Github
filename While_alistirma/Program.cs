f33082d1efd0ad4994502ec63dcd9ba2c7a9e26c
lordkales <akifboylu_93@hotmail.com>
2023-01-04 07:30:01 +0300

    while alıştırması eklendi

---
+++ While_alistirma/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //1'den 10'a kadar sayıları ekrana yazdır.
+        int a =1;
+        while (a<=10)
+        {
+            Console.WriteLine(a);
+            a++;
+        }
+
+        //100'den 1'e kadar olan çift sayıları büyükten küçüğe yazdır
+
+        int b =100;
+        while (b>=0)
+        {
+            Console.WriteLine(b);
+            b-=2;
+        }
+    }
+}
