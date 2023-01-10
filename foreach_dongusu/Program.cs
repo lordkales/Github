57047bacfa10dcc1bd9aec7452abe0a323c8f9b5
Burak <akifboylu_93@hotmail.com>
2023-01-10 14:28:53 +0300

    commitlendi

---
+++ foreach_dongusu/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string [] isimler = {"akif","tuğçe","ela"};
+        
+        foreach (var item in collection)
+        {
+            
+        }
+    }
+}
