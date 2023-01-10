d3cda838366b8b334a3e565701dd5e7e5bd8430d
Burak <akifboylu_93@hotmail.com>
2023-01-10 14:31:13 +0300

    commitlendi

--- Hackerrank_birtday_cake_candles/Program.cs
+++ Hackerrank_birtday_cake_candles/Program.cs
@@ -3,7 +3,16 @@
     private static void Main(string[] args)
     {
         int mumsayisi = Convert.ToInt32(Console.ReadLine());
-
+        
+        for (int i = 0; i < mumsayisi; i++)
+        {
+            int mumgir = Convert.ToInt16(Console.ReadLine());
+            int buyukmum = Math.Max(mumgir,mumsayisi);
+        }
+        
+        /*int [] mum = new int [3];
+        int mumboyu = Convert.ToInt32(Console.ReadLine());
+        int uzunmum = mumboyu.max();*/
         
 
     }
