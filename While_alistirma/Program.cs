5e7375e22b67bb2b24f01b7fa6880093fbae5c28
Burak <akifboylu_93@hotmail.com>
2023-01-13 10:56:26 +0300

    alıştırmalar commitlendi

--- While_alistirma/Program.cs
+++ While_alistirma/Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
         //1'den 10'a kadar sayıları ekrana yazdır.
-        int a =1;
+        /*int a =1;
         while (a<=10)
         {
             Console.WriteLine(a);
@@ -17,6 +17,25 @@
         {
             Console.WriteLine(b);
             b-=2;
-        }
+        }*/
+
+        //1'den 10'a kadar sayıları ekrana yazdır. (do while ile)
+        int a=1;
+        do
+        {
+            Console.WriteLine(a);
+            a++;
+        } while (a<=10);
+
+        //1'den 100'e kadar olan sayıların toplamı yazdır (do while ile)
+
+        int toplam=0,b=1;
+        
+        do
+        {
+            toplam=toplam+b;
+            b++;
+        } while (b<=100);
+        Console.WriteLine(toplam);
     }
 }
