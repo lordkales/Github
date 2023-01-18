ca49d08c495ac10f3dae80945a336f2f613f01e0
lordkales <akifboylu_93@hotmail.com>
2023-01-18 22:08:55 +0300

    Patika Metot dersi commitlendi

---
+++ metot_Tanimlama/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       int a =2, b=3;
+       int returntoplam=Toplam(a,b);
+       Console.WriteLine(returntoplam);
+
+       metotlar ornek = new metotlar();
+       ornek.ekranayazdir(Convert.ToString(returntoplam));
+    }
+
+    static int Toplam(int sayi1, int sayi2)
+    {
+        return sayi1+sayi2;
+    }
+}
+
+class metotlar
+{
+    public void ekranayazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+}
