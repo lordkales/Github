644059e51452f384eee13626057887f11101a731
lordkales <akifboylu_93@hotmail.com>
2023-01-19 07:21:11 +0300

    overloading tekrar edildi

---
+++ metot_Overloading/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Out parametreler
+
+        string sayi = "999";
+
+        bool sonuc = int.TryParse(sayi, out int outsayi );
+        if (sonuc)
+        {
+            Console.WriteLine("BAŞARILI");
+            Console.WriteLine(outsayi);
+        }
+        else
+        {
+            Console.WriteLine("BAŞARISIZ");
+        }
+
+        // Method Overloading
+
+        Console.WriteLine(multiply(2,3));   
+        Console.WriteLine(multiply(4,5,6));   
+    }
+    static int multiply(int number1, int number2)
+    {
+        return number1*number2;
+    }
+    static int multiply(int number1, int number2, int number3)
+    {
+        return number1*number2*number3;
+    }
+
+}
