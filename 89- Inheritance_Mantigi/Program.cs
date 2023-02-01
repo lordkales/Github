209b2b1719c8d2af0ba1d2e39fc1642a39125bd1
lordkales <akifboylu_93@hotmail.com>
2023-02-01 23:56:24 +0300

    commitlendi

---
+++ 89- Inheritance_Mantigi/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        kedi k1 = new kedi();
+        k1.hayvan_cinsiyet="Erkek";
+        k1.hayvan_ismi="Osman";
+        k1.hayvan_rengi="Boz";
+        k1.hayvanbilgisigoster();
+        
+    }
+    public class hayvan
+    {
+        public string hayvan_ismi;
+        public string hayvan_cinsiyet;
+        public string hayvan_rengi;
+        private int hayvan_yas;
+
+        public void hayvanbilgisigoster()
+        {
+            Console.WriteLine("Hayvanın ismi: "+hayvan_ismi+" Hayvan Cinsiyeti: "+ hayvan_cinsiyet+" Hayvan Rengi: "+hayvan_rengi);
+        }
+    }
+
+    class kedi : hayvan
+    {
+
+    }
+}
---
+++ 90- Classlar_Arasi_Inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       ArrayList arraylist;
+    }
+    public class hayvan
+    {
+        public string hayvan_isim;
+        public string hayvan_renk;
+        public int hayvan_kilo;
+
+    }
+    public class kopek : hayvan
+    {
+        public int kopek_ayaksayisi;
+        public string kopek_cinsiyet;
+
+    }
+    public class kedi : kopek
+    {
+        public string kedi_sesi;
+        public void kedi_yazdir()
+        {
+            Console.WriteLine("Kedi Sesi: "+kedi_sesi);
+            Console.WriteLine("Hayvan İsmi: "+hayvan_isim);
+            Console.WriteLine("Hayvan Rengi: "+hayvan_renk);
+            Console.WriteLine("Hayvan Kilo: "+hayvan_kilo);
+            Console.WriteLine("Köpek Ayak Sayısı: "+kopek_ayaksayisi);
+            Console.WriteLine("Köpek Cinsiyet: "+kopek_cinsiyet);
+        }
+    }
+}
